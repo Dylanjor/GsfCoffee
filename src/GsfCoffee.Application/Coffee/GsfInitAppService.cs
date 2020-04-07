912d937703e567a6c6b107b7976d33bc0e4c7e71
礼家 代 <2545664565@qq.com>
2020-04-07 10:02:58 +0800

    修改类名

---
+++ src/GsfCoffee.Application/Coffee/GsfInitAppService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsfCoffee.Coffee
+{
+    public class GsfInitAppService: ApplicationService,IGsfInitAppService
+    {
+        public int main { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 主方法
+        /// </summary>
+        public async Task<int> Main() {
+            return (1 + 2);
+        }
+    }
+}
---
+++ src/GsfCoffee.Application/Coffee/IGsfInitAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GsfCoffee.Coffee
+{
+    public interface IGsfInitAppService
+    {
+        int main { get; set; }
+    }
+}
