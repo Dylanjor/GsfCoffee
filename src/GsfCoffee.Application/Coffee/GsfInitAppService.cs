170eaa358ac6906515749717596851b847d2fa53
礼家 代 <2545664565@qq.com>
2020-04-07 12:05:45 +0800

    登录和注册

--- src/GsfCoffee.Application/Coffee/GsfInitAppService.cs
+++ src/GsfCoffee.Application/Coffee/GsfInitAppService.cs
@@ -1,22 +1,26 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
 using GsfCoffee.CoffeeUser;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GsfCoffee.Coffee
 {
-    public class GsfInitAppService: GsfCoffeeAppServiceBase,IGsfInitAppService
+    public class GsfInitAppService : GsfCoffeeAppServiceBase, IGsfInitAppService
     {
         private readonly IRepository<UserTable> _repository;
+
+        Task IGsfInitAppService.Register { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        Task<ListResultDto<UserTable>> IGsfInitAppService.Login { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         public GsfInitAppService(IRepository<UserTable> repository) {
             _repository = repository;
         }
-
-        int IGsfInitAppService.Main { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         [HttpPost]
         /// <summary>
         /// 注册信息
@@ -25,6 +29,19 @@ namespace GsfCoffee.Coffee
         {
             var usertable = ObjectMapper.Map<UserTable>(_userTable);
             await _repository.InsertAsync(usertable);
+        }
+        /// <summary>
+        /// 查询信息 登录
+        /// </summary>
+        /// <param name="userid">用户名</param>
+        /// <param name="pwd">用户密码</param>
+        /// <returns></returns>
+        public async Task<ListResultDto<UserTable>> Login(string userid,string pwd)
+        {
+            var usertable = _repository
+                .GetAll()
+                .Where(c=>c.Name == userid && c.PassWord == pwd);
+            return new ListResultDto<UserTable>(ObjectMapper.Map<List<UserTable>>(usertable));
         }
     }
 }
--- src/GsfCoffee.Application/Coffee/IGsfInitAppService.cs
+++ src/GsfCoffee.Application/Coffee/IGsfInitAppService.cs
@@ -1,12 +1,22 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using GsfCoffee.CoffeeUser;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GsfCoffee.Coffee
 {
     public interface IGsfInitAppService: IApplicationService
     {
-       int Main { get; set; }
+        /// <summary>
+        /// 注册的接口
+        /// </summary>
+        Task Register { get; set; }
+        /// <summary>
+        /// 登陆的接口
+        /// </summary>
+        Task<ListResultDto<UserTable>> Login { get; set; }
     }
 }
