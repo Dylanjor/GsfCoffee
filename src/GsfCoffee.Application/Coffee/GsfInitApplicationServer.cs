487bf50d8dd8ce9c3b3f5b0dfdbafbb61f2eaf7a
礼家 代 <2545664565@qq.com>
2020-04-07 09:07:51 +0800

    初始化

---
+++ src/GsfCoffee.Application/Coffee/GsfInitApplicationServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GsfCoffee.Coffee
+{
+    public class GsfInitApplicationServer
+    {
+        /// <summary>
+        /// 主方法
+        /// </summary>
+        public int Main() {
+            return (1 + 2);
+        }
+    }
+}
---
+++ src/GsfCoffee.Core/CoffeeUser/UserTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GsfCoffee.CoffeeUser
+{
+    public class UserTable
+    {
+        [Required]
+        /// <summary>
+        /// 账户姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 账户密码
+        /// </summary>
+        public string PassWord { get;set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public string Resource { get; set; }
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public DateTime? Date { get; set; }
+        /// <summary>
+        /// 店面
+        /// </summary>
+        public string Region { get; set; }
+        /// <summary>
+        /// 是否是vip
+        /// </summary>
+        public bool Delivery { get; set; }
+        /// <summary>
+        /// 爱好
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? UpdateTime { get; set; }
+        /// <summary>
+        /// 是否弃用
+        /// </summary>
+        public bool Deprecated { get; set; }
+        /// <summary>
+        /// 弃用时间
+        /// </summary>
+        public DateTime? DeprecatedTime { get; set; }
+    }
+}
